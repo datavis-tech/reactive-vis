117a09f94dc72036eac78de520748dcd4e2a2035
Curran <curran.kelleher@gmail.com>
2016-06-06 00:42:44 +0530

    initial columns mixin tests. Towards #6

--- test/mixins/columnTest.js
+++ test/mixins/columnTest.js
@@ -6,15 +6,15 @@ module.exports = function (common){
 
   describe("Column", function(){
     it("Should create a column property with name prefix.", function(){
-      //var my = ReactiveVis.Model()
-      //  .call(ReactiveVis.Data)
-      //  .call(ReactiveVis.Column, "x");
+      var my = ReactiveVis.Model()
+        .call(ReactiveVis.Data)
+        .call(ReactiveVis.Column, "x");
 
-      //my.xColumn("foo");
-      //assert.equal(my.xColumn(), "foo");
-      //
-      //output("column");
-      //my.destroy();
+      my.xColumn("foo");
+      assert.equal(my.xColumn(), "foo");
+      
+      output("column");
+      my.destroy();
     });
   });
 };
