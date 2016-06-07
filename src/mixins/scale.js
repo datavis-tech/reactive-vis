c81774ca42fe2efdbaa353e630c699fb4274b0c2
Curran <curran.kelleher@gmail.com>
2016-06-07 11:40:33 +0530

    Move issues out of comments and into GitHub issues.

--- src/mixins/scale.js
+++ src/mixins/scale.js
@@ -10,30 +10,13 @@ export default function Scale (my, name, options){
 
   options = options || {};
   var type = options.type || "linear";
-  var nice = options.nice || true;
 
   var scale = scaleConstructors[type]();
-
-
-  // (name + "Domain") and (name + "Range")
-  // must be defined on the model before incoking this mixin.
-  // TODO throw an error if they are not defined at this point.
-  // GitHub issue #25 https://github.com/datavis-tech/reactive-vis/issues/25
 
   my
 
     (name + "Scale", function(domain, range){
-
-      scale
-        .domain(domain)
-        .range(range);
-
-      // TODO test
-      //if(nice){
-      //  scale.nice();
-      //}
-
-      return scale;
+      return scale.domain(domain).range(range);
     }, [name + "Domain", name + "Range"])
 
     (name + "Scaled", function(scale, accessor){
