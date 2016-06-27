0ee93e337a686ada8afe1666f087c29df2391e36
Curran <curran.kelleher@gmail.com>
2016-06-27 11:16:13 +0530

    Clear gh-pages

--- src/mixins/column.js
+++
@@ -1,6 +0,0 @@
-export default function Column (my, name){
-  my(name + "Column")
-    (name + "Accessor", function (column){
-      return function (d){ return d[column]; };
-    }, name + "Column");
-}
--- src/mixins/data.js
+++
@@ -1,3 +0,0 @@
-export default function Data (my){
-  my("data");
-}
